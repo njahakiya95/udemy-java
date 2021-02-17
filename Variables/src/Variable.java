372745e7f412a06572f22622c1f797af60c1fbc2
Nirmal Patel <patelnirmal1325@gmail.com>
2021-02-17 18:18:39 -0500

    finished Variable.java file

--- Variables/src/Variable.java
+++ Variables/src/Variable.java
@@ -1,3 +1,17 @@
 public class Variable {
+    public static void main(String[] args) {
+        //Create three number variables
+        int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
 
+        //Sum up all three numbers and store in total
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        //Subtract 1000 from myTotal and store in myLastOne
+        int myLastOne = 1000 - myTotal;
+
+        //Output 1000 - myTotal
+        System.out.println(myLastOne);
+    }
 }
