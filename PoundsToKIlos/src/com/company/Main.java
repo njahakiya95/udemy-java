c46306f75a818accd5a1b3b597ecc0a2ccd6fe66
Nirmal Patel <patelnirmal1325@gmail.com>
2021-02-18 15:22:24 -0500

    added PoundsToKilos

---
+++ PoundsToKIlos/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double Pounds = 150;
+        double Kilograms = Pounds * 0.45359237;
+        System.out.println(Pounds + " pounds in kilograms is " + Kilograms);
+    }
+}
