a17d07b5ba31cbd416765cffc850220504c8bf64
Nirmal Patel <patelnirmal1325@gmail.com>
2021-02-18 21:01:16 -0500

    added String folder

---
+++ String/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String myString = "This is a string";
+        myString = myString + " and this is also a string!";
+        System.out.println("myString is equal to " + myString);
+    }
+}
