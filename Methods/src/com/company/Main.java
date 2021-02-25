81575638b1230a77c6a3836081dc91fd861e3eb6
Nirmal Patel <patelnirmal1325@gmail.com>
2021-02-24 21:46:01 -0500

    modified Methods main file

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -3,24 +3,37 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
         boolean gameOver = true;
         int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
 
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
+    }
+
+    public static void displayHighScorePosition(String playName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position "
+                + highScorePosition + " on the high score table");
+    }
+
+    public static int calculatoeHighScorePosition(int playerScore) {
+        if (playerScore > 1000) {
+            return 1;
+        } else if (playerScore > 500 && playerScore < 1000) {
+            return 3;
+        } else if (playerScore > 100 && playerScore < 500) {
+            return 4;
+        }
+     }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
+            return finalScore;
         }
-
-        score = 1000;
-        levelCompleted = 8;
-        bonus = 200;
-
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
+        return -1;
     }
 }
