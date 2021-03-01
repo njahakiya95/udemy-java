f3418170e76c9662d90b91e07d52b59361cf9ea8
Nirmal Patel <patelnirmal1325@gmail.com>
2021-03-01 17:39:29 -0500

    finished BarkingDog project

--- BarkingDog/src/BarkingDog.java
+++ BarkingDog/src/BarkingDog.java
@@ -1,13 +1,13 @@
 public class BarkingDog {
     public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
         if (hourOfDay < 0 || hourOfDay > 23) {
-            return True;
+            return false;
         }
         else if (barking && hourOfDay < 8 || hourOfDay > 22) {
-            return False;
+            return true;
         }
         else {
-            return False;
+            return false;
         }
     }
 }
---
+++ BarkingDog/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void Main(String[] args) {
+        BarkingDog.shouldWakeUp(true, 1);
+    }
+}
