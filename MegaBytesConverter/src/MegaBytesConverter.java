7c5d8097c6f112c28a399e1c273ad71a65419f5f
Nirmal Patel <patelnirmal1325@gmail.com>
2021-03-01 14:09:26 -0500

    started MegaBytesConverter project

---
+++ MegaBytesConverter/src/MegaBytesConverter.java
@@ -0,0 +1,10 @@
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        }
+        else {
+            int megaBytes = kiloBytes % 1024;
+        }
+    }
+}
