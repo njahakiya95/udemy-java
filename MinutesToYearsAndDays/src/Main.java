adff6e79f785ecb2827f65ac16629693e503650c
Nirmal Patel <patelnirmal1325@gmail.com>
2021-03-02 11:52:14 -0500

    finished MinutesToYearAndDays project

---
+++ MinutesToYearsAndDays/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        MinutesToYearsAndDays.printYearsAndDays(525600);
+    }
+}
---
+++ MinutesToYearsAndDays/src/MinutesToYearsAndDays.java
@@ -0,0 +1,12 @@
+public class MinutesToYearsAndDays {
+    public static void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long year = (minutes / 525600);
+            long days = ((minutes % 525600) / 1440);
+
+            System.out.println(minutes + " min == " + year + " y and " + days + " d ");
+        }
+    }
+}
