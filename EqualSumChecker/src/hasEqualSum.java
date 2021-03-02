70f30e787dd87478643f1af240f32cbb0af66ee5
Nirmal Patel <patelnirmal1325@gmail.com>
2021-03-01 21:48:22 -0500

    added EqualSumChecker

---
+++ EqualSumChecker/src/hasEqualSum.java
@@ -0,0 +1,9 @@
+public class hasEqualSum {
+    public static boolean hasEqualSum(int numOne, int numTwo, int numThree) {
+        if ((numOne + numTwo) == numThree) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
