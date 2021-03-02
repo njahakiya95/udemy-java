81650148b5506fd8433d37960878e581ae1dcf21
Nirmal Patel <patelnirmal1325@gmail.com>
2021-03-02 11:29:39 -0500

    added AreaCalculator project

---
+++ AreaCalculator/src/AreaCalculator.java
@@ -0,0 +1,17 @@
+public class AreaCalculator {
+    public static double Area(double radius) {
+        if (radius < 0.0) {
+            return -1.0;
+        } else {
+            return (Math.PI * radius * radius);
+        }
+    }
+
+    public static double Area(double x, double y) {
+        if (x < 0 || y < 0) {
+            return -1.0;
+        } else {
+            return (x * y); 
+        }
+    }
+}
