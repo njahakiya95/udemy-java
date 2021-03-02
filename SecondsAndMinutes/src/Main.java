25e6c7d64afada9400f12a7756cc3658db8d7adf
Nirmal Patel <patelnirmal1325@gmail.com>
2021-03-02 11:06:07 -0500

    updated SecondsAndMinutes project files

--- SecondsAndMinutes/src/Main.java
+++ SecondsAndMinutes/src/Main.java
@@ -7,8 +7,20 @@ public class Main {
         if ((minutes < 0) || (seconds < 0) || (seconds > 59)) {
             return "Invalid Value";
         }
+
         long hours = minutes / 60;
         long remainingMinutes = minutes % 60;
         return (hours + "h " + remainingMinutes + "m " + seconds + "s");
+    }
+
+    private static String getDurationString(long seconds) {
+        if (seconds < 0) {
+            return "Invalid value";
+        }
+
+        long minutes = seconds / 60;
+        long remainingSeconds = seconds % 60;
+
+        return getDurationString(minutes, seconds); 
     }
 }
