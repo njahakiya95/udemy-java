0dd091d9a542ca36377378098b4fcc0a8a66db2d
Nirmal Patel <patelnirmal1325@gmail.com>
2021-03-05 13:27:49 -0500

    finished NumberOfDaysInMonth project

--- NumberOfDaysInMonth/src/Main.java
+++ NumberOfDaysInMonth/src/Main.java
@@ -1,15 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-
+        isLeapYear(-1600);
+        isLeapYear(1600);
+        getDaysInMonth(1, 2020);
+        getDaysInMonth(2, 2018);
     }
 
     public static boolean isLeapYear(int year) {
-        if ( ((year >= 1) && (year <= 9999)) && (((year % 4 == 0) || (year % 100 != 0)) || (year % 400 == 0)) ) {
+        if ( (year < 1) || (year > 9999)) {
+            return false;
+        } else if ( ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0) ) {
             return true;
         } else {
             return false;
         }
     }
 
-    public static
+    public static int getDaysInMonth(int month, int year) {
+        if ( ((month < 1) || (month > 12)) || ((year < 1) || (year > 9999)) ) {
+            return -1;
+        } else if ( isLeapYear(year) && (month == 2)) {
+            return 29;
+        } else {
+            switch (month) {
+                case 2:
+                    return 28;
+                case 4: case 6: case 9: case 11:
+                    return 30;
+                default:
+                    return 31;
+            }
+        }
+    }
 }
