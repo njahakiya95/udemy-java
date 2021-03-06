54a528543f08cd52b049124b61d66e3d8859c599
Nirmal Patel <patelnirmal1325@gmail.com>
2021-03-06 16:12:16 -0500

    added SumOddRange project

---
+++ SumOddRange/src/SumOddRange.java
@@ -0,0 +1,12 @@
+public class SumOddRange {
+    public static boolean isOdd(int number) {
+        if (number <= 0) {
+            return false;
+        } else return number % 2 != 0;
+    }
+
+    public static int sumOdd(int start, int end) {
+        int sum = 0;
+        if ()
+    }
+}
