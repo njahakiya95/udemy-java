6c99b51aa9bae987c0db24ae507f4cdbdba99790
Nirmal Patel <patelnirmal1325@gmail.com>
2021-03-08 23:07:12 -0500

    added FirstAndLastDigitSum

---
+++ FIrstAndLastDigitSum/src/FirstAndLastDigitSum.java
@@ -0,0 +1,14 @@
+public class FirstAndLastDigitSum {
+    public static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int lastDigit = number % 10;
+        while (number >= 10) {
+            number /= 10;
+        }
+
+        return lastDigit + number;
+    }
+}
