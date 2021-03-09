8b2e61bd580bcf4b7ab986edb4dfae4d55fce015
Nirmal Patel <patelnirmal1325@gmail.com>
2021-03-09 00:04:47 -0500

    added EvenDigitSUm project

---
+++ EvenDigitSum/src/EvenDigitSum.java
@@ -0,0 +1,10 @@
+public class EvenDigitSum {
+    public static int getEvenDigitSum(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int num = number;
+
+    }
+}
