a7095dfef57aa998394e1af727e9fef44caff7fe
Nirmal Patel <patelnirmal1325@gmail.com>
2021-03-11 13:38:59 -0500

    added Greatest Common Divisor project

--- GreatestCommonDivisor/src/GreatestCommonDivisor.java
+++ GreatestCommonDivisor/src/GreatestCommonDivisor.java
@@ -4,5 +4,12 @@ public class GreatestCommonDivisor {
             return -1;
         }
 
+        int gcd = 0;
+        for (int i = 1; i <= first && i <= second; i++) {
+            if (first % i == 0 && second % i == 0) {
+                gcd = i;
+            }
+        }
+        return gcd;
     }
 }
