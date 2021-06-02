af3c2e2360fcb8662759662eaaac9cefeddcd6a6
Nirmal Patel <patelnirmal1325@gmail.com>
2021-06-01 22:57:49 -0400

    modified BillsBUrgers project

--- BillsBurgers/src/DeluxeBurger.java
+++ BillsBurgers/src/DeluxeBurger.java
@@ -7,4 +7,24 @@ public class DeluxeBurger extends Hamburger {
         super.addHamburgerAddition2("Drink", 0);
     }
 
+    //DeluxeBurger class methods
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("No additional items can be added to a deluxe burger.");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("No additional items can be added to a deluxe burger.");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("No additional items can be added to a deluxe burger.");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("No additional items can be added to a deluxe burger.");
+    }
 }
--- BillsBurgers/src/HealthyBurger.java
+++ BillsBurgers/src/HealthyBurger.java
@@ -1,2 +1,31 @@
-public class HealthyBurger {
+public class HealthyBurger extends Hamburger {
+
+    //HealthyBurger class instances
+    private String healthyExtra1Name;
+    private String healthyExtra2Name;
+
+    private double healthyExtra1Price;
+    private double healthyExtra2Price;
+
+    //HealthyBurger class constructor
+    public HealthyBurger(String meat, double price) {
+        super("Beyond Burger", meat, price, "Honeywheat Bread");
+    }
+
+    //HealthyBUrger class methods
+    public void addHealthyAddition1(String name, double price) {
+        healthyExtra1Name = name;
+        healthyExtra1Price = price;
+    }
+
+    public void addHealthyAddition2(String name, double price) {
+        healthyExtra2Name = name;
+        healthyExtra2Price = price;
+    }
+
+    @Override
+    public double itemizeHamburger() {
+        System.out.println("One order of Beyond Burger on Honeywheat Bread with " + healthyExtra1Name + " " + healthyExtra2Name);
+        return (super.itemizeHamburger() + healthyExtra1Price + healthyExtra2Price);
+    }
 }
